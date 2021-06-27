a4aea255ff05a8c1d42933343d206aa6e17501e2
Cristian Vázquez <crvh2688@gmail.com>
2021-06-27 15:45:54 -0500

    Register user

--- js/registerUser.js
+++ js/registerUser.js
@@ -4,10 +4,10 @@ $(function(){
         $( "#registerForm" ).submit(function( event ) {
             event.preventDefault();
 
-        var name = $("#nameInput").val();
-        var lastname = $("#lastnameInput").val();
-        var email = $("#registerEmailInput").val();
-        var password = $("#registerPasswordInput").val();
+        var name =      $("#nameInput").val();
+        var lastname =  $("#lastnameInput").val();
+        var email =     $("#registerEmailInput").val();
+        var password =  $("#registerPasswordInput").val();
 
         var invalidFeedback= "invalid-feedback";
         var blockTooltip = "displayBlockToolTip";
@@ -19,29 +19,28 @@ $(function(){
         $("#passwordToolTip").removeClass(invalidFeedback).removeClass(blockTooltip).empty();
 
 
-        if(!name){
+        if(!name)
             $("#nameToolTip").addClass(invalidFeedback).addClass(blockTooltip).append("El Nombre es requerido");
             console.log("name");
-        }
 
-        if(!lastname){
+        if(!lastname)
             $("#lastnameToolTip").addClass(invalidFeedback).addClass(blockTooltip).append("El Apellido Paterno es requerido");
-            console.log("lastname");
-        }
 
-        if(!email){
+        if(!email)
             $("#emailToolTip").addClass(invalidFeedback).addClass(blockTooltip).append("El Email es requerido");
-            console.log("email invalido");
-        }
 
-        if(!password){
+        if(!password)
             $("#passwordToolTip").addClass(invalidFeedback).addClass(blockTooltip).append("El Password es requerido");
-            console.log("password invalido");
-        }
 
     });
 
     $( "#closeRegisterUser" ).click(function( event ) {
+
+        $("#nameInput").val("");
+        $("#lastnameInput").val("");
+        $("#registerEmailInput").val("");
+        $("#registerPasswordInput").val("");
+
 
         var invalidFeedback= "invalid-feedback";
         var blockTooltip = "displayBlockToolTip";
