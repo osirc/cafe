0adcc7034fe5f8ecd329dd79b1d7815c7b32908b
Cristian Vázquez <crvh2688@gmail.com>
2021-06-27 00:26:56 -0500

    Register user

--- registerUser.php
+++ registerUser.php
@@ -11,49 +11,56 @@
             <div class="modal-body">
                 <div class="container-fluid">
                     <form>
-                        <div class="form-row">
-                            <div class="form-group col-md-6">
-                                <label for="inputEmail4">Email</label>
-                                <input type="email" class="form-control" id="inputEmail4">
-                            </div>
-                            <div class="form-group col-md-6">
-                                <label for="inputPassword4">Password</label>
-                                <input type="password" class="form-control" id="inputPassword4">
-                            </div>
-                        </div>
                         <div class="form-group">
-                            <label for="inputAddress">Address</label>
-                            <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+                            <label for="nameInput">Name</label>
+                            <div class="input-group pb-md-2 pt-md-2">
+                                <span class="input-group-prepend">
+                                    <div class="input-group-text bg-transparent border-right-0">
+                                        <i class="fa fa-user" aria-hidden="true"></i>
+                                    </div>
+                                </span>
+                                <input class="form-control py-2 border-left-0 border" type="text" id="nameInput">
+                            </div>
                         </div>
+
                         <div class="form-group">
-                            <label for="inputAddress2">Address 2</label>
-                            <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-                        </div>
-                        <div class="form-row">
-                            <div class="form-group col-md-6">
-                                <label for="inputCity">City</label>
-                                <input type="text" class="form-control" id="inputCity">
-                            </div>
-                            <div class="form-group col-md-4">
-                                <label for="inputState">State</label>
-                                <select id="inputState" class="form-control">
-                                    <option selected>Choose...</option>
-                                    <option>...</option>
-                                </select>
-                            </div>
-                            <div class="form-group col-md-2">
-                                <label for="inputZip">Zip</label>
-                                <input type="text" class="form-control" id="inputZip">
+                            <label for="nameInput">Lastname</label>
+                            <div class="input-group pb-md-2 pt-md-2">
+                                <span class="input-group-prepend">
+                                    <div class="input-group-text bg-transparent border-right-0">
+                                        <i class="fa fa-user" aria-hidden="true"></i>
+                                    </div>
+                                </span>
+                                <input class="form-control py-2 border-left-0 border" type="text" id="lastnameInput">
                             </div>
                         </div>
+
+
                         <div class="form-group">
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" id="gridCheck">
-                                <label class="form-check-label" for="gridCheck">
-                                    Check me out
-                                </label>
+                            <label for="nameInput">Email</label>
+                            <div class="input-group pb-md-2 pt-md-2">
+                                <span class="input-group-prepend">
+                                    <div class="input-group-text bg-transparent border-right-0">
+                                        <i class="fa fa-envelope" aria-hidden="true"></i>
+                                    </div>
+                                </span>
+                                <input class="form-control py-2 border-left-0 border" type="search" id="emailInput">
                             </div>
                         </div>
+
+
+                        <div class="form-group">
+                            <label for="nameInput">Password</label>
+                            <div class="input-group pb-md-2 pt-md-2">
+                                <span class="input-group-prepend">
+                                    <div class="input-group-text bg-transparent border-right-0">
+                                        <i class="fa fa-lock" aria-hidden="true"></i>
+                                    </div>
+                                </span>
+                                <input class="form-control py-2 border-left-0 border" type="password" id="passwordInput">
+                            </div>
+                        </div>
+
                         <button type="submit" class="btn btn-primary float-right">Sign in</button>
 
                     </form>
